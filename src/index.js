8b99cb72cf7a41798f49fd276904b0adf88dcabd
Aleksandr Sobolev <aleksandr_sobolev2@epam.com>
2025-05-14 10:48:54 +0400

    add function to repo

---
+++ src/index.js
@@ -0,0 +1,33 @@
+const { SNSClient, PublishCommand } = require('@aws-sdk/client-sns');
+
+const snsClient = new SNSClient();
+
+exports.handler = async (event) => {
+  console.log('Received event:', JSON.stringify(event, null, 2));
+
+  const topicArn = process.env.SNS_TOPIC_ARN;
+
+  const messages = event.Records.map(record => {
+    try {
+      return JSON.parse(record.body);
+    } catch (e) {
+      return { error: "Invalid JSON in SQS message body", raw: record.body };
+    }
+  });
+
+  const message = `Uploaded files metadata:\n${JSON.stringify(messages, null, 2)}`;
+
+  const params = {
+    Message: message,
+    TopicArn: topicArn,
+  };
+
+  const command = new PublishCommand(params);
+
+  try {
+    await snsClient.send(command);
+    console.log("Message published to SNS.");
+  } catch (err) {
+    console.error("Failed to publish message", err);
+  }
+};
